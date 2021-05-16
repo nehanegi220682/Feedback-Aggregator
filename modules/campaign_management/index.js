dcdb6ef79f3e6e9b45c9ae3b27e2d2b2d48b18ec
Yatin darmwal <yatin@infeedo.com>
2021-05-16 18:35:18 +0530

    toogle campaign done api done with authorization

--- modules/campaign_management/index.js
+++ modules/campaign_management/index.js
@@ -17,6 +17,18 @@ protected_router.post('/create_campaign', async (req, res) => {
     }
 });
 
+protected_router.post('/change_status', async (req, res) => {
+    try {
+        const new_campaign_status = req.body;
+        await campaign_services.toggleCampaignStatus(new_campaign_status, req.customer);
+        return res.send(`Campaign Status Changed to :: ${new_campaign_status.status}`);
+    } catch (err) {
+        if (err.code == APP_ERROR_CODES.INFORMATIVE_ERROR)
+            return res.status(HTTP_STATUS_CODES.INVALID_INPUT).send(err.message);
+        return res.status(HTTP_STATUS_CODES.GENERIC_SERVER_ERROR).send('Something Went Wrong');
+    }
+});
+
 module.exports = {
     protected_router
 };
--- modules/campaign_management/services.js
+++ modules/campaign_management/services.js
@@ -10,10 +10,39 @@ const createCampaign = async (campaign, customer) => {
         await _validateCampaign(campaign);
         let current_usage = await _checkUsageLimit(customer.id);
         campaign = _serializeCustomer(campaign, customer);
-        await saveCampaign(campaign);
-        await updateUsageCount(current_usage, customer.id);
+        await _saveCampaign(campaign);
+        await _updateUsageCount(current_usage, customer.id);
     } catch (err) {
         if (err.message) err.code = APP_ERROR_CODES.INFORMATIVE_ERROR;
+        throw err;
+    }
+}
+
+
+const toggleCampaignStatus = async (new_campaign_status, customer) => {
+    try {
+        _validateToggleInput(new_campaign_status);
+        let { campaign_id, status } = new_campaign_status;
+        await _isAuthorizedToEditCampaign(campaign_id, customer.id);
+        await _updateCampaignStatus(status, campaign_id);
+    } catch (err) {
+        if (err.message) err.code = APP_ERROR_CODES.INFORMATIVE_ERROR;
+        throw err;
+    }
+}
+
+const _validateToggleInput = (new_campaign_status) => {
+    try {
+        let { campaign_id, status } = new_campaign_status;
+        if (!(new_campaign_status && Object.keys(new_campaign_status).length))
+            throw { message: 'campaign is required in body' };
+        if (!campaign_id)
+            throw { message: 'campaign_id is a required field' };
+        if (!status)
+            throw { message: 'status id is a required field' };
+        if (!(status == 'ACTIVE' || status == 'PAUSED'))
+            throw { message: 'Not a valid status' };
+    } catch (err) {
         throw err;
     }
 }
@@ -37,7 +66,7 @@ const _serializeCustomer = (campaign, customer) => {
     } catch (err) { throw err }
 }
 
-const saveCampaign = async (campaign) => {
+const _saveCampaign = async (campaign) => {
     try {
         let new_campaign = new Campaign(campaign);
         await new_campaign.save();
@@ -54,13 +83,31 @@ const _checkUsageLimit = async (customer_id) => {
     } catch (err) { throw err }
 }
 
-const updateUsageCount = async (current_usage, customer_id) => {
+const _updateUsageCount = async (current_usage, customer_id) => {
     try {
         current_usage++;
-        await Customer.FindAndModify({ _id: customer_id }, { campaign_limit_used: current_usage });
+        let res = await Customer.findOneAndUpdate({ _id: customer_id }, { campaign_limit_used: current_usage });
+        if (!res) throw { message: 'Customer not found' };
+    } catch (err) { throw err }
+}
+
+const _isAuthorizedToEditCampaign = async (campaign_id, customer_id) => {
+    try {
+        let campaign = await Campaign.findById({ _id: campaign_id });
+        if (!campaign) throw { message: 'campaign not available' };
+        if (campaign.customer_id.id.toString('hex') == customer_id) return true;
+        throw { message: 'Unauthorized to toggle this campaign' };
+    } catch (err) { throw err }
+}
+
+const _updateCampaignStatus = async (status_to_update, campaign_id) => {
+    try {
+        let res = await Campaign.findOneAndUpdate({ _id: campaign_id }, { campaign_status: status_to_update });
+        if (!res) throw { message: 'campaign not available' };
     } catch (err) { throw err }
 }
 
 module.exports = {
-    createCampaign
+    createCampaign,
+    toggleCampaignStatus
 }
